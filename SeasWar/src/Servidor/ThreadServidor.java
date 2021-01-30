c2f98d6ee39a90c264ff13149f2ae1a92b8e032a
RavenInDisguise <monicaalfarop@estudiantec.cr>
2021-01-30 02:01:37 -0600

    Correcion de }

--- SeasWar/src/Servidor/ThreadServidor.java
+++ SeasWar/src/Servidor/ThreadServidor.java
@@ -206,6 +206,7 @@ class ThreadServidor extends Thread{
                             current.writer.writeUTF(mensajeRetorno5);
                             current.writer.writeInt(4);
                             current.writer.writeUTF("Turno de ataque: "+siguiente2);
+                        }
                     break;
                     case 10:
                         String datos3=reader.readUTF();
